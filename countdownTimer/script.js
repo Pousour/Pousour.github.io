74d363565ec30fda85dea03ce1022908f124993a
Pousour <60235943+Pousour@users.noreply.github.com>
2020-12-04 11:28:51 +0100

    update timer

--- countdownTimer/script.js
+++ countdownTimer/script.js
@@ -1,5 +1,7 @@
 loadEventListeners();
 
+document.getElementById("date").value = "2021-01-01";
+
 function loadEventListeners() {
   document.addEventListener("DOMContentLoaded", function() {
     calcTemps();
@@ -9,8 +11,10 @@ function loadEventListeners() {
 var timeTo = document.getElementById("date").value,
   date,
   maintenant,
-  nouvelleAnnee = new Date("1.1.2020").getTime(),
+  nouvelleAnnee = "2021-01-01",
   startTimer = "";
+
+calcTemps("2021-01-01");
 
 function calcTemps(dates) {
   clearInterval(startTimer);
@@ -20,34 +24,35 @@ function calcTemps(dates) {
   } else {
     date = new Date(dates).getTime();
   }
-
-  function updateTimer(date) {
-    var maintenant = new Date().getTime();
-    var distance = date - maintenant;
-
-    var jours = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var heures = Math.floor(
-      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var secondes = Math.floor((distance % (1000 * 60)) / 1000);
-
-    document.querySelector(".temps-jours").innerHTML = jours;
-    document.querySelector(".temps-heures").innerHTML = heures;
-    document.querySelector(".temps-minutes").innerHTML = minutes;
-    document.querySelector(".temps-secondes").innerHTML = secondes;
-
-    if (maintenant >= date) {
-      clearInterval(startTimer);
-
-      document.querySelector(".temps-jours").innerHTML = "F";
-      document.querySelector(".temps-heures").innerHTML = "I";
-      document.querySelector(".temps-minutes").innerHTML = "N";
-      document.querySelector(".temps-secondes").innerHTML = "I";
-    }
-  }
-
   startTimer = setInterval(function() {
     updateTimer(date);
   }, 1000);
 }
+
+
+function updateTimer(date) {
+  var maintenant = new Date().getTime();
+  var distance = date - maintenant;
+
+  var jours = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var heures = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var secondes = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.querySelector(".temps-jours").innerHTML = jours;
+  document.querySelector(".temps-heures").innerHTML = heures;
+  document.querySelector(".temps-minutes").innerHTML = minutes;
+  document.querySelector(".temps-secondes").innerHTML = secondes;
+
+  if (maintenant >= date) {
+    clearInterval(startTimer);
+
+    document.querySelector(".temps-jours").innerHTML = "F";
+    document.querySelector(".temps-heures").innerHTML = "I";
+    document.querySelector(".temps-minutes").innerHTML = "N";
+    document.querySelector(".temps-secondes").innerHTML = "I";
+  }
+}
+
